9798597426e18a5f7d963fdda37473651b9fe134
justi <justingaming800@gmail.com>
2024-12-03 21:45:44 -0600

    Created world map for game. Seperates the game into tiles that can be filled w/ information which will choose what image to fill inside the tile. Changed game resolution for ease of implementation.

--- src/maingame.py
+++ src/maingame.py
@@ -1,34 +1,91 @@
 import pygame
 from pygame.locals import *
 
+#Intializes system information/debug.
+resolution = (750, 750)
+screen = pygame.display.set_mode(resolution)
+pygame.display.set_caption('To the Stars!')
+tile_size = 50
+
+#Loads in necessary images
+bg_img = pygame.image.load('bg_img.jpg')
+
+def draw_grid():
+    for line in range(0, 15):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (750, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, 750))
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #Loads in necessary images
+        cloud_img = pygame.image.load('cloud.png.png')
+        star_img = pygame.image.load('star.png.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(cloud_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(star_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+
+
+world_data = [
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
+]
+
 
 def main():
-   #Intializes pygame and screen information
-   pygame.init()
-   resolution = (1920, 1080)
-   screen = pygame.display.set_mode(resolution)
-   pygame.display.set_caption('To the Stars!')
+    pygame.init()
+    world = World(world_data)
+    running = True
+    while running:
+        screen.blit(bg_img, (0,0))
+        world.draw()
+        draw_grid()
 
-   #Loads in necessary images
-
-   bg_img = pygame.image.load('bg_img.jpg')
-
-
-   running = True
-   while running:
-      screen.blit(bg_img, (0,0))
-
-
-      for event in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            pygame.display.update()
-
-pygame.quit()
-
+        pygame.display.update()
+    
+    pygame.quit()
 
 if __name__ == "__main__":
-    main()
-
-    
+    main() 
